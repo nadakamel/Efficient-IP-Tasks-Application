1db90db859273d4fd45edcc7d5d0e6f1fa14c5c8
Nada Kamel <nadakamel9@hotmail.com>
2014-01-25 01:42:57 +0200

    Create ContrastForm.Designer.cs

---
+++ ContrastForm.Designer.cs
@@ -0,0 +1,318 @@
+namespace Efficient_IP_Tasks
+{
+    partial class ContrastForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNormalContrast = new System.Windows.Forms.Button();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.contrastTrackBar = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.contrastTextBox = new System.Windows.Forms.TextBox();
+            this.MainMenu_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSave_Click = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_MaxWindowSize = new System.Windows.Forms.TextBox();
+            this.btnEfficientContrast = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contrastTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(563, 430);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(619, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(563, 430);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(226, 499);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(104, 76);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open Image";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 462);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(814, 462);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contrasted Image";
+            // 
+            // btnNormalContrast
+            // 
+            this.btnNormalContrast.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNormalContrast.Location = new System.Drawing.Point(710, 498);
+            this.btnNormalContrast.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNormalContrast.Name = "btnNormalContrast";
+            this.btnNormalContrast.Size = new System.Drawing.Size(178, 73);
+            this.btnNormalContrast.TabIndex = 5;
+            this.btnNormalContrast.Text = "Normal Contrast Image";
+            this.btnNormalContrast.UseVisualStyleBackColor = true;
+            this.btnNormalContrast.Click += new System.EventHandler(this.btnNormalContrast_Click);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Enabled = false;
+            this.txtWidth.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWidth.Location = new System.Drawing.Point(310, 609);
+            this.txtWidth.Margin = new System.Windows.Forms.Padding(4);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(89, 27);
+            this.txtWidth.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(181, 613);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Image Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(180, 661);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Image Height";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Enabled = false;
+            this.txtHeight.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHeight.Location = new System.Drawing.Point(310, 657);
+            this.txtHeight.Margin = new System.Windows.Forms.Padding(4);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(89, 27);
+            this.txtHeight.TabIndex = 8;
+            // 
+            // contrastTrackBar
+            // 
+            this.contrastTrackBar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.contrastTrackBar.Location = new System.Drawing.Point(1232, 79);
+            this.contrastTrackBar.Margin = new System.Windows.Forms.Padding(4);
+            this.contrastTrackBar.Maximum = 100;
+            this.contrastTrackBar.Minimum = -100;
+            this.contrastTrackBar.Name = "contrastTrackBar";
+            this.contrastTrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.contrastTrackBar.Size = new System.Drawing.Size(56, 388);
+            this.contrastTrackBar.TabIndex = 10;
+            this.contrastTrackBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1189, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Contrast Degree";
+            // 
+            // contrastTextBox
+            // 
+            this.contrastTextBox.Enabled = false;
+            this.contrastTextBox.Location = new System.Drawing.Point(1230, 50);
+            this.contrastTextBox.Name = "contrastTextBox";
+            this.contrastTextBox.Size = new System.Drawing.Size(58, 22);
+            this.contrastTextBox.TabIndex = 15;
+            this.contrastTextBox.Text = "0";
+            this.contrastTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainMenu_button
+            // 
+            this.MainMenu_button.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenu_button.Location = new System.Drawing.Point(1198, 629);
+            this.MainMenu_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MainMenu_button.Name = "MainMenu_button";
+            this.MainMenu_button.Size = new System.Drawing.Size(124, 62);
+            this.MainMenu_button.TabIndex = 24;
+            this.MainMenu_button.Text = "Back to Main Menu";
+            this.MainMenu_button.UseVisualStyleBackColor = true;
+            this.MainMenu_button.Click += new System.EventHandler(this.MainMenu_button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(971, 612);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 53);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "Execution Time Graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnContrastGraph_Click);
+            // 
+            // buttonSave_Click
+            // 
+            this.buttonSave_Click.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave_Click.Location = new System.Drawing.Point(1193, 502);
+            this.buttonSave_Click.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSave_Click.Name = "buttonSave_Click";
+            this.buttonSave_Click.Size = new System.Drawing.Size(116, 76);
+            this.buttonSave_Click.TabIndex = 30;
+            this.buttonSave_Click.Text = "Save Image";
+            this.buttonSave_Click.UseVisualStyleBackColor = true;
+            this.buttonSave_Click.Click += new System.EventHandler(this.buttonSave_Click_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(606, 626);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(238, 20);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Enter Maximum Window Size :";
+            // 
+            // textBox_MaxWindowSize
+            // 
+            this.textBox_MaxWindowSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_MaxWindowSize.Location = new System.Drawing.Point(845, 625);
+            this.textBox_MaxWindowSize.Name = "textBox_MaxWindowSize";
+            this.textBox_MaxWindowSize.Size = new System.Drawing.Size(100, 27);
+            this.textBox_MaxWindowSize.TabIndex = 31;
+            // 
+            // btnEfficientContrast
+            // 
+            this.btnEfficientContrast.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEfficientContrast.Location = new System.Drawing.Point(937, 499);
+            this.btnEfficientContrast.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEfficientContrast.Name = "btnEfficientContrast";
+            this.btnEfficientContrast.Size = new System.Drawing.Size(177, 76);
+            this.btnEfficientContrast.TabIndex = 33;
+            this.btnEfficientContrast.Text = "Efficient Contrast Image";
+            this.btnEfficientContrast.UseVisualStyleBackColor = true;
+            this.btnEfficientContrast.Click += new System.EventHandler(this.btnEfficientContrast_Click);
+            // 
+            // ContrastForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 708);
+            this.Controls.Add(this.btnEfficientContrast);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_MaxWindowSize);
+            this.Controls.Add(this.buttonSave_Click);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MainMenu_button);
+            this.Controls.Add(this.contrastTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.contrastTrackBar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.btnNormalContrast);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ContrastForm";
+            this.Text = "Image Contrast";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contrastTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNormalContrast;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TrackBar contrastTrackBar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox contrastTextBox;
+        private System.Windows.Forms.Button MainMenu_button;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSave_Click;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_MaxWindowSize;
+        private System.Windows.Forms.Button btnEfficientContrast;
+    }
+}
+
